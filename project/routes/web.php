444d36b0ceb8ba67683011953f1561677692c1cf
Shibnath Roy <shibnath.roy.007@gmail.com>
2019-08-17 13:41:32 +0530

    First commit : configured laravel websockets and checked channel, events

---
+++ project/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use App\Events\WebsocketDemoEvent;
+
+Route::get('/', function () {
+    broadcast(new WebsocketDemoEvent('some data'));
+    return view('welcome');
+});
